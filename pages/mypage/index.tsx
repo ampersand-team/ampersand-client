d0723c21dc85696b38841d3f73a80446f5ef71dd
bae-sh <hyeon1293@gmail.com>
2023-02-08 19:54:00 -0500

    feat : 프로필 이미지 기본값 설정"

--- pages/mypage/index.tsx
+++ pages/mypage/index.tsx
@@ -6,36 +6,57 @@ import {
   Text,
   Wrap,
   WrapItem,
+  Image,
 } from "@chakra-ui/react";
 import Layout from "components/Layout";
 import { useRef, useState } from "react";
+import mypic from "../../picture.jpg";
 
 export default function MyPage() {
   const [settable, isSettable] = useState(false);
-  const inputRef = useRef(null);
+  const [profileImage, setProfileImage] = useState<string | null>(null);
+  const inputRef = useRef<HTMLInputElement>(null);
+  const imageRef = useRef<HTMLImageElement>(null);
 
+  const imageClick = () => {
+    if (inputRef.current) inputRef.current.click();
+  };
+  const imageChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const file = event.target.files?.[0];
+    if (file) {
+      const reader = new FileReader();
+      reader.onload = (e) => {
+        if (e.target?.result) {
+          setProfileImage(e.target.result as string);
+        }
+      };
+      reader.readAsDataURL(file);
+    }
+  };
   return (
     <Layout>
       <Flex flexDirection="column" align="center" marginTop="30px" gap="20px">
         <Heading as="h1" size="xl" noOfLines={1}>
           내 정보
         </Heading>
-        <Button
-          width="200px"
-          height="200px"
-          borderRadius="50%"
-          onClick={() => inputRef.current.click()}
-        >
-          <Input
-            type="file"
-            ref={inputRef}
-            display="none"
-            accept="image/*"
-            onChange={(e) => {
-              console.log(e);
-            }}
-          />
-        </Button>
+        <Image
+          borderRadius="full"
+          boxSize="200px"
+          ref={imageRef}
+          onClick={imageClick}
+          src={`${profileImage || mypic.src}`}
+          alt="프로필 이미지"
+          _hover={{ cursor: "pointer", opacity: 0.8 }}
+          backgroundPosition="center"
+          backgroundSize="cover"
+        />
+        <input
+          type="file"
+          ref={inputRef}
+          accept="image/*"
+          onChange={imageChange}
+          style={{ display: "none" }}
+        />
         {/* <Wrap>
           <WrapItem width="500px">
             <Text fontSize="xl">닉네임 : </Text>
